caabe25b14ded9b1d57ede422e7ba048df6a54e7
Henry Cordes <137107811+Hcordes6@users.noreply.github.com>
2026-08-02 18:43:18 +0200

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict_s = {}
+        dict_t = {}
+        for i in s:
+            if i not in dict_s:
+                dict_s.update({i : 0})
+            else:
+                dict_s.update({i : dict_s[i] + 1})
+        for i in t:
+            if i not in dict_t:
+                dict_t.update({i : 0})
+            else:
+                dict_t.update({i : dict_t[i] + 1})
+        if dict_s == dict_t:
+            return True
+        return False
